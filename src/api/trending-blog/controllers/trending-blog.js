4807daba3fe12505101690b10e3991c6dc51b375
Emanuel Christo <emanuelchristochris@gmail.com>
2024-03-15 20:56:43 +0530

    initial commit

---
+++ src/api/trending-blog/controllers/trending-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trending-blog controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trending-blog.trending-blog');
---
+++ src/api/trending-blog/routes/trending-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trending-blog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trending-blog.trending-blog');
